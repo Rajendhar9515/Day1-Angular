5af404a1ac9e17a2a2c5d60e6e1f34cba92eb5f0
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-10-21 15:21:27 +0530

    added Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello';
+  imgUrl = "../assets/bridgelabz.jpg";
+  Url = "https://www.bridgelabz.com";
+  name = "";
+  nameError = "";
+
+ ngOnInit(): void {
+    this.title = "Hello from Bridgelabz";
+  }
+  onClick($event:any){
+    console.log("button clicked", $event)
+    window.open(this.Url, "blank")
+  }
+  onInput($event:any){
+    console.log ("chenge event occurd",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-z]{2,}$');
+    if (nameRegex.test(this.name)) {
+      this.nameError = "";
+      return;
+    } else {
+      this.nameError = "Name is InCorrect";
+    }
+  }
 }
